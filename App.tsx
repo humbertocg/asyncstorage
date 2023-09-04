23f3b88f9a7cf0817ee2e4aa026f2d9c532e28c3
Humberto Castañeda Garcia <humbertocg@gmail.com>
2023-09-04 16:31:18 -0600

    added async storage package and consumed

--- App.tsx
+++ App.tsx
@@ -1,117 +1,108 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import React from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import React, {useEffect, useState} from 'react';
 import type {PropsWithChildren} from 'react';
 import {
+  Button,
   SafeAreaView,
   ScrollView,
   StatusBar,
   StyleSheet,
   Text,
+  TextInput,
+  TouchableHighlight,
   useColorScheme,
   View,
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+const App = () => {
+  const [inputTexto, setInputTexto] = useState('');
+  const [nombreStorage, setNombreStorage] = useState<any>('');
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
-
-function App(): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  const guardarDato = async () => {
+    try {
+      await AsyncStorage.setItem('name', inputTexto);
+      setInputTexto('');
+      setNombreStorage(inputTexto);
+    } catch (ex) {}
   };
 
+  const obtenerDato = async () => {
+    try {
+      const dato = await AsyncStorage.getItem('name');
+      setNombreStorage(dato);
+    } catch (ex) {
+      throw ex;
+    }
+  };
+
+  const elminarDato = async () => {
+    await AsyncStorage.removeItem('name');
+    await obtenerDato();
+  };
+
+  useEffect(() => {
+    obtenerDato();
+  }, [nombreStorage]);
+
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
+    <SafeAreaView style={styles.contenedor}>
+      <ScrollView contentContainerStyle={[styles.contenedor, styles.controls]}>
+        {nombreStorage && <Text>Hola: {nombreStorage}</Text>}
+        <TextInput
+          value={inputTexto}
+          style={styles.input}
+          placeholder="Escribe tu nombre"
+          onChangeText={text => {
+            setInputTexto(text);
+          }}
+        />
+        <Button
+          title="Guardar"
+          color={'#333'}
+          onPress={() => {
+            guardarDato();
+          }}
+        />
+        {nombreStorage && (
+          <TouchableHighlight
+            style={styles.btnEliminar}
+            onPress={() => {
+              elminarDato();
+            }}>
+            <Text style={styles.textoEliminar}>Eliminar Nombre &times;</Text>
+          </TouchableHighlight>
+        )}
       </ScrollView>
     </SafeAreaView>
   );
-}
+};
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  contenedor: {
+    flex: 1,
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  controls: {
+    backgroundColor: '#FFF',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
+  input: {
+    borderColor: '#666',
+    borderBottomWidth: 1,
+    width: 300,
+    height: 40,
   },
-  highlight: {
-    fontWeight: '700',
+  btnEliminar: {
+    backgroundColor: 'red',
+    marginTop: 20,
+    padding: 10,
+  },
+  textoEliminar: {
+    color: '#FFF',
+    textTransform: 'uppercase',
+    fontWeight: 'bold',
+    textAlign: 'center',
+    width: 300,
   },
 });
 
